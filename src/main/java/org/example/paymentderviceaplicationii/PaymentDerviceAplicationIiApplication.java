3d4aebd57c8a35c0e81aa99759052cf358f484ff
Andrej Romanov <romanov454545@gmail.com>
2025-06-08 13:40:36 +0300

    First commit

---
+++ src/main/java/org/example/paymentderviceaplicationii/PaymentDerviceAplicationIiApplication.java
@@ -0,0 +1,13 @@
+package org.example.paymentderviceaplicationii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentDerviceAplicationIiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentDerviceAplicationIiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/paymentderviceaplicationii/PaymentDerviceAplicationIiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paymentderviceaplicationii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentDerviceAplicationIiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
