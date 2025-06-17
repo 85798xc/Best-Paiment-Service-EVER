e85c61d3da2c519dab0e5593893bb58247fabff8
ArnasGlo <arnas.globys@gmail.com>
2025-06-17 10:40:44 +0300

    g ipisau vietoj q

--- src/test/java/org/example/paymentderviceaplicationii/CucumberConfig.java
+++ src/test/java/org/example/paymentderviceaplicationii/CucumberConfig.java
@@ -12,7 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-classes = {PaymentDerviceAplicationIiApplication.class,MockUsersConfiq.class})
+classes = {PaymentDerviceAplicationIiApplication.class, MockUsersConfig.class})
 @AutoConfigureMockMvc
 @CucumberContextConfiguration
 public class CucumberConfig {
